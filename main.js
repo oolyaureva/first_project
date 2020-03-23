0ae0d316365362ff614538090111315cf302b552
Olya Yurieva <olyaureva1@gmail.com>
2020-03-23 16:17:58 +0200

    Add main.css and main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $(".mblBtn").on("click",function(){
+  	$(".headerBottomWrap").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+  	loop:true,
+  	items:1,
+  	dots:true,
+  });
+});
+
+
+
